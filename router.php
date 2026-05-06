3882a0bfda3bb6b29f2a9a2f530de5c9edf1ab6d
Tinnixss <valentina.paixao@estudante.ifto.edu.br>
2026-05-06 11:51:28 -0300

    STUDYFLOW | rota

--- router.php
+++ router.php
@@ -4,14 +4,18 @@ require_once 'middleware.php';
 class Router {
     private EstudanteController $controller;
 
-    public function __construct($controller) {
+    // O construtor recebe o controller montado (Injeção de Dependência)
+    public function __construct(EstudanteController $controller) {
         $this->controller = $controller;
     }
 
     public function handle() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             try {
+                // Chama o Middleware para sanitizar os dados
                 $dadosLimpos = Middleware::sanitizar($_POST);
+                
+                // Passa os dados limpos para o Controller
                 $GLOBALS['mensagem_feedback'] = $this->controller->store($dadosLimpos);
             } catch (Exception $e) {
                 $GLOBALS['mensagem_feedback'] = "❌ " . $e->getMessage();
